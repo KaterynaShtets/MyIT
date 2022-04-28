c1200f5adec9723d1522cb6fd617e44a9f0fab4e
siehe <oleh.shmelov@nure.ua>
2022-04-28 17:24:39 +0300

    auth controller

--- MyIT.API/Controllers/AuthenticationController.cs
+++ MyIT.API/Controllers/AuthenticationController.cs
@@ -1,12 +1,30 @@
+using Amazon;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
 using Microsoft.AspNetCore.Mvc;
+using MyIT.Contracts;
 
 namespace MyIT.API.Controllers;
 
-public class Authentication : Controller
+[ApiController]
+[Route("api/auth")]
+public class AuthenticationController : Controller
 {
-    // GET
-    public IActionResult Index()
-    {
-        return View();
+    [HttpPost]
+    public async Task<IActionResult> Authenticate([FromBody] UserProfile user){
+        var cognito = new AmazonCognitoIdentityProviderClient(RegionEndpoint.GetBySystemName("eu-west-1"));
+
+        var request = new InitiateAuthRequest
+        {
+            ClientId = "1hcjcjgumctg2ud7vetrbj0hra",
+            AuthFlow = AuthFlowType.USER_PASSWORD_AUTH
+        };
+
+        request.AuthParameters.Add("USERNAME", user.Username);
+        request.AuthParameters.Add("PASSWORD", user.Password);
+
+        var response = await cognito.InitiateAuthAsync(request);
+
+        return Ok(response.AuthenticationResult);
     }
 }
--- MyIT.Contracts/UserProfile.cs
+++ MyIT.Contracts/UserProfile.cs
@@ -2,5 +2,7 @@ namespace MyIT.Contracts;
 
 public class UserProfile
 {
-    
+    public string Username { get; set; }
+
+    public string Password { get; set; }
 }
