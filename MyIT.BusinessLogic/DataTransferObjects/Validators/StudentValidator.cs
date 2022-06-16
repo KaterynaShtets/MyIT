de190390a38c1865def719060be83754800bfb1e
Rostyslav Tukalo <Rostyslav_Tukalo@jabil.com>
2022-06-16 16:41:49 +0300

    validation fix

--- MyIT.BusinessLogic/DataTransferObjects/Validators/StudentValidator.cs
+++ MyIT.BusinessLogic/DataTransferObjects/Validators/StudentValidator.cs
@@ -8,19 +8,6 @@ public sealed class StudentValidator : AbstractValidator<StudentDto>
     {
         RuleFor(b => b.FullName).MinimumLength(1).MaximumLength(100);
         RuleFor(b => b.Email).EmailAddress();
-        RuleFor(b => b.DOB).NotEmpty()
-            .LessThan(p => DateTime.Now).Must(BeAValidAge);
-    }
-
-    private bool BeAValidAge(DateTime date){
-        int currentYear = DateTime.Now.Year;
-        int dobYear = date.Year;
-
-        if (dobYear < (currentYear -16) && dobYear > (currentYear - 120)){
-            return true;
-        }
-
-        return false;
     }
 }
 
